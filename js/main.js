411ed82a16f994a9bb6b0224bb054b59c7b4dc98
Nicola <niconix.dev@gmail.com>
2020-04-06 20:36:32 +0200

    Controllo

--- js/main.js
+++ js/main.js
@@ -7,8 +7,12 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+
 var mine = [];
-var number;
+var numInseriti = [];
+var number = 0;
 for (var i = 0; i < 16; i++) {
     number = Math.floor(Math.random() * 100) + 1;
 
@@ -19,17 +23,21 @@ for (var i = 0; i < 16; i++) {
     }
 }
 
-console.log(mine);
+
 
 for (var i = 0; i < 16; i++) {
-    var richiesta = parseInt(prompt('Inserisi i numeri'));
+
+    var richiesta = parseInt(prompt('Inserisi un numero da 1 a 100'));
 
     if (mine.includes(richiesta)) {
         console.log("Hai perso");
         break;
+    } else if (numInseriti.includes(richiesta)) {
+        richiesta = parseInt(prompt('Inserisi un numero che non hai inserito in precedenza'));
     } else {
-        mine.push(richiesta);
+        numInseriti.push(richiesta);
     }
 }
 
 console.log(mine);
+console.log(numInseriti);
